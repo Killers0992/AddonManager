b54c5c5a726e206555cf2a5ee5ac991f5828f4dc
Killers0992 <killers0992@gmail.com>
2022-01-04 21:12:13 +0100

    Up

--- AddonManager/AddonManagerHandler.cs
+++ AddonManager/AddonManagerHandler.cs
@@ -2,6 +2,7 @@
 {
     using NetworkedPlugins.API;
     using NetworkedPlugins.API.Events.Player;
+    using NetworkedPlugins.API.Events.Server;
     using System;
 
     public class AddonManagerHandler : NPAddonHandler<AddonConfig>
@@ -10,7 +11,25 @@
         {
             this.PlayerJoined += OnPlayerJoin;
             this.PlayerLeft += OnPlayerLeft;
+            this.PlayerPreAuth += OnPreAuth;
+            this.WaitingForPlayers += OnWaitingForPlayers;
+            this.RoundEnded += OnRoundEnded;
             base.OnEnable();
+        }
+
+        private void OnRoundEnded(RoundEndedEvent ev)
+        {
+            Logger.Info($"Round ended on server \"{ev.Server.FullAddress}\".");
+        }
+
+        private void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+        {
+            Logger.Info($"Waiting for players on server \"{ev.Server.FullAddress}\".");
+        }
+
+        private void OnPreAuth(PlayerPreAuthEvent ev)
+        {
+            Logger.Info($"Player preauth \"{ev.UserID}\" country \"{ev.Country}\" from server \"{ev.Server.FullAddress}\".");
         }
 
         private void OnPlayerLeft(PlayerLeftEvent ev)
@@ -27,6 +46,9 @@
         {
             this.PlayerJoined -= OnPlayerJoin;
             this.PlayerLeft -= OnPlayerLeft;
+            this.PlayerPreAuth -= OnPreAuth;
+            this.WaitingForPlayers -= OnWaitingForPlayers;
+            this.RoundEnded -= OnRoundEnded;
             base.OnDisable();
         }
     }
